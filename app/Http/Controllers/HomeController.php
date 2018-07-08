7483310eecbf818cf6ea3d720210012944c5c605
sho23 <testuser.shota@gmail.com>
2018-07-08 20:15:29 +0900

    ルーティングを修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,8 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
+        $this->middleware('auth', ['except' => 'getExits']);
     }
 
     /**
